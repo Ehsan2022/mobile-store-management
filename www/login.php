6a295c1ce4fd4cddba0f277cad482bde66ca3130
Mohammad Ehsan Nicksarisht <ehsan.nick16@gmail.com>
2024-07-10 03:53:52 +0430

    login page and sidebar updated

--- www/login.php
+++ www/login.php
@@ -43,28 +43,30 @@
 
 <body>
 
-  <main class="bg-dark">
+  <main >
     <div class="container ">
 
       <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
-        <div class="container">
+        <div class="container ">
           <div class="row justify-content-center">
-            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-
-              <div class="d-flex justify-content-center py-4">
+            <div class="col-lg-4  col-md-6 d-flex flex-column align-items-center justify-content-center">
+                      <div class="col-12 ">
+                        <a href="/php/Mobily/index.php" class="btn btn-primary w-100 " type="submit" name="logged" value="Login">Back To Home</a>
+                      </div>
+              <div class="d-flex justify-content-center py-4 ">
                 <a href="index.html" class="logo d-flex align-items-center w-auto">
                   <img src="assets/img/myLogo.png" alt="">
-                  <span class="d-none d-lg-block">E-SMS</span>
+                  <span class="d-none d-lg-block text-dark">E-SMS</span>
                 </a>
               </div><!-- End Logo -->
 
-              <div class="card mb-3">
-
-                <div class="card-body">
+              <div class="card mb-3 bg-dark bg-gradient text-white">
+                     
+                  <div class="card-body">
 
                   <div class="pt-4 pb-2">
-                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                    <p class="text-center small">Enter your username & password to login</p>
+                    <h5 class="card-title text-center pb-0 fs-4 text-white">Login to Your Account</h5>
+                    <p class="text-center text-warning">Only E-SMS Admins can login</p>
                   </div>
 
                   <form class="row g-3 needs-validation" novalidate method="post" action="#">
@@ -99,7 +101,7 @@
               </div>
 
               <div class="credits">
-                <span class="text-white">Designed by</span> <a href="https://emen.com/"><strong class="text-primary">Mohammad Ehsan Nicksaresht</strong></a>
+                <span >Designed by</span> <a href="https://emen.com/"><strong class="text-primary">Mohammad Ehsan Nicksaresht</strong></a>
               </div>
 
             </div>
